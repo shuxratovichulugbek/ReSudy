3d3999c7f846686e3fabf216c8d320698248dceb
sherzod <abdumutalovsherzod96@gmail.com>
2022-12-14 17:20:59 +0500

    User and role models created and doe

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,8 +32,6 @@ class User extends Authenticatable
     ];
 
 
-    public function role()
-    {
-        return $this->belongsTo(Role::class);
-    }
+    public function role(){return $this->belongsTo(Role::class);}
+    public function applications(){return $this->hasMany(Application::class);}
 }
---
+++ database/migrations/2022_12_14_120735_create_applications_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicationsTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('subject');
+            $table->text('message');
+            $table->text('file_url');
+            $table->timestamps();
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+}
